5dc1524588ee3418c9721704c0cab1e06bfddd31
vaskaloidis <vas.kaloidis@gmail.com>
2018-10-20 00:59:26 -0400

    init

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+gem 'middleman', :git => 'https://github.com/middleman/middleman.git'
+# gem 'middleman', '~> 4.2'
+gem 'middleman-autoprefixer'
+gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
+gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw]
---
+++ source/javascripts/google_maps.js
@@ -0,0 +1,45 @@
+/* ---- Google Maps ---- */
+function initMap() {
+    var mapOptions = {
+        zoom: 15,
+        zoomControl: false,
+        scaleControl: false,
+        scrollwheel: false,
+        disableDoubleClickZoom: true,
+        center: new google.maps.LatLng(950575, -82.45717760000002), // New York
+        styles: [{
+            "featureType": "landscape",
+            "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]
+        }, {
+            "featureType": "poi",
+            "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]
+        }, {
+            "featureType": "road.highway",
+            "stylers": [{"saturation": -100}, {"visibility": "simplified"}]
+        }, {
+            "featureType": "road.arterial",
+            "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]
+        }, {
+            "featureType": "road.local",
+            "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]
+        }, {
+            "featureType": "transit",
+            "stylers": [{"saturation": -100}, {"visibility": "simplified"}]
+        }, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {
+            "featureType": "water",
+            "elementType": "labels",
+            "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]
+        }, {
+            "featureType": "water",
+            "elementType": "geometry",
+            "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]
+        }]
+    };
+    var mapElement = document.getElementById('map');
+    var map = new google.maps.Map(mapElement, mapOptions);
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(27.929517, -82.511970),
+        map: map,
+        title: 'Located in Tampa, Florida!'
+    });
+}
